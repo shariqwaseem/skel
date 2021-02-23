c1b46e793f43fa8389a16808ece00cb6761717d3
Shariq <sh4riqw@gmail.com>
2021-02-23 17:52:20 +0500

    Redux provider added

--- Main.js
+++ Main.js
@@ -2,9 +2,12 @@ import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+
+//React navigation
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import DummyScreen from "./Screens/DummyScreen";
+//React navigation
 
 // Redux includes and inits
 import store from "./store";
@@ -14,28 +17,27 @@ import { Provider } from "react-redux";
 let persistor = persistStore(store);
 //Redux includes and inits
 
-
-
 //Firebase init
 import * as firebase from "firebase";
 import { firebaseConfig } from "./config/firebase";
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
-
-
 //Firebase init
-
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="DummyScreen" component={DummyScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="DummyScreen" component={DummyScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PersistGate>
+    </Provider>
   );
 };
 export default App;
--- Screens/DummyScreen.js
+++ Screens/DummyScreen.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React ,{useState, useEffect} from "react";
 import { View, Text } from "react-native";
+import {useSelector} from 'react-redux'
+import {Button} from 'react-native-elements'
 const DummyScreen = (props) => {
+	const [loading, setLoading] = useState(false)
+	const appName = useSelector(state=>{
+		return state.Misc.name
+	})
+	useEffect(()=>{
+		console.log('state', appName)
+	},[])
 	return (
 		<View
 			style={{
@@ -9,7 +18,16 @@ const DummyScreen = (props) => {
 				alignItems: "center",
 			}}
 		>
-			<Text>Dummy Screen</Text>
+		<Button
+			loading={loading}
+			type="clear"
+			title="okay"
+			onPress={()=>{
+				setLoading(true)
+				setTimeout(()=>setLoading(false), 3000)
+				}}
+		/>
+			<Text>{appName}</Text>
 		</View>
 	);
 };
